92659567989ecd2da98eaa53ac59a24fda677057
thelemon2020 <thelemon.chris@gmail.com>
2026-04-29 15:07:28 -0400

    fix failing tests

--- Pest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-uses(Thelemon2020\PestPom\Tests\TestCase::class)->in('tests');
---
+++ tests/Pest.php
@@ -0,0 +1,5 @@
+<?php
+
+declare(strict_types=1);
+
+pest()->extend(Thelemon2020\PestPom\Tests\TestCase::class)->in('.');
