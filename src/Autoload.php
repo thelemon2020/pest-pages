b5bafed898de2fca7790398a6c78cff5d88f6425
thelemon2020 <thelemon.chris@gmail.com>
2026-05-05 09:01:44 -0400

    fix global function issue

--- src/Autoload.php
+++ src/Autoload.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Thelemon2020\PestPom;
-
 require_once __DIR__.'/Expectations.php';
+
+use Thelemon2020\PestPom\Page;
 
 /**
  * Navigate to a page object by class name and return the typed instance.
